30625ccfbec4bb6f43fba9b26b7a47b6db7511c6
Marina Santana <marinasantana7894@gmail.com>
2020-12-09 20:57:58 -0300

    feat: animação do posicionamento do card

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -11,18 +11,24 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   bool _showMenu;
   int _currentIndex;
+  double _yPosition;
 
     @override
   void initState() {
     super.initState();
     _showMenu = false;
     _currentIndex = 0;
+    _yPosition = 0;
   }
 
   @override
   Widget build(BuildContext context) {
     double _screenHeigth = MediaQuery.of(context).size.height;
 
+    if(_yPosition == null){
+      _yPosition = _screenHeigth * .24;
+    }
+    
     return Scaffold(
       backgroundColor: Color(0xffA332B3),
       body: Stack(
@@ -33,18 +39,59 @@ class _HomePageState extends State<HomePage> {
             onTap: () {
               setState(() {
                 _showMenu = !_showMenu;
-                
+                _yPosition =
+                    _showMenu ? _screenHeigth * .75 : _screenHeigth * .24;
               });
             },
           ),
           PageViewApp(
-            top: _screenHeigth * .24,
-            onChanged: (index) {
-              setState(() {
-                _currentIndex = index;
-              });
-            },
-          ),
+              top: _yPosition,
+              showMenu: _showMenu,
+              onChanged: (index) {
+                setState(() {
+                  _currentIndex = index;
+                });
+              },
+              onPanUpdate: (details){
+                double positionBottomLimit = _screenHeigth * .75;
+                double positionTopLimit = _screenHeigth * .24;
+                double midlePosition = positionBottomLimit - positionTopLimit;
+                midlePosition = midlePosition / 2;
+
+                setState(() {
+                  _yPosition += details.delta.dy;
+
+                  _yPosition = _yPosition < positionTopLimit
+                      ? positionTopLimit
+                      : _yPosition;
+
+                  _yPosition = _yPosition > positionBottomLimit
+                      ? positionBottomLimit
+                      : _yPosition;
+
+                    
+                  });
+                    
+                  if (_yPosition == positionBottomLimit) {
+                    _showMenu = true;
+                  } else if (_yPosition == positionTopLimit) {
+                    _showMenu = false;
+                  }
+
+                   if (_yPosition != positionBottomLimit && details.delta.dy > 0) {
+                  _yPosition =
+                      _yPosition > positionTopLimit + midlePosition - 50
+                          ? positionBottomLimit
+                          : _yPosition;
+                    }
+
+                    if (_yPosition != positionTopLimit && details.delta.dy < 0) {
+                      _yPosition = _yPosition < positionBottomLimit - midlePosition
+                          ? positionTopLimit
+                          : _yPosition;
+                    }
+              },
+            ),
           Positioned(
             top: _screenHeigth * .70,
             child: MyDotsApp(currentIndex: _currentIndex)
